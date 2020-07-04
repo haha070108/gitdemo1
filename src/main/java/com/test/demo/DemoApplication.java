3827a353c4d6c320abffb4e6ec9ae8845b80fc6f
zhuyf@mti-sh.cn <zhuyafei>
2020-07-04 11:49:16 +0800

    this is feature-test1

--- src/main/java/com/test/demo/DemoApplication.java
+++ src/main/java/com/test/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
-    	System.out.println("sss");
+    	System.out.println("this feature-test1");
         SpringApplication.run(DemoApplication.class, args);
     }
 
